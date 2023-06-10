485483660189563ba96dd3749edce427d266278d
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-10 17:42:28 +0300

    add Home_Works_6

---
+++ Home_Works_№6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.Write(num + " четное число");
+}
+else
+{
+    Console.Write(num + " нечетное число");
+}
