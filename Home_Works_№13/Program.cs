13a1c5613e5e9fb50cf0bde3b913a4b62fe953dc
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-15 15:55:38 +0300

    add_Home_Works_15

---
+++ Home_Works_№13/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число ");
+int a = int.Parse(Console.ReadLine());
+int b = 0;
+while (a != 0)
+{
+    b = (b * 10) + (a % 10);
+    a = a / 10;
+}
+Console.WriteLine(b);
---
+++ Home_Works_№15/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.Write("Введи целое число: ");
+int numberA = int.Parse(Console.ReadLine());
+switch (numberA)
+{
+    case 1: 
+    Console.Write("Сегодня рабочий день ");
+        break;
+      
+      case 2: 
+    Console.Write("Сегодня рабочий день ");
+        break;
+    
+      case 3: 
+    Console.Write("Сегодня рабочий день ");
+        break;
+
+      case 4: 
+    Console.Write("Сегодня рабочий день ");
+        break;
+
+      case 5: 
+    Console.Write("Сегодня рабочий день ");
+        break;    
+
+      case 6: 
+    Console.Write("Сегодня выходной ");
+        break;
+
+      case 7: 
+    Console.Write("Сегодня выходной ");
+        break;    
+    
+    default:
+    Console.Write("в недели 7 дней, попробуй снова  ");
+     break;
+
+}
