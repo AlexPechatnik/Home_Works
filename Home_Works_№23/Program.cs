9dea18312afc89a4f6db2b959780b78032453ea7
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-22 16:12:04 +0300

    add_home_works_23

--- Home_Works_№23/Program.cs
+++ Home_Works_№23/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+int temp = n;
+    while (i<=n)
+    {
+        int result = i*i*i;
+        i++;
+        Console.Write($"{result}, ");
+    }
+
+
