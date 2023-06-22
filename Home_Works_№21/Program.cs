7138af5d43013f91cda226618680d11b34edb32c
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-22 16:15:26 +0300

    add_home_works_21

--- Home_Works_№21/Program.cs
+++ Home_Works_№21/Program.cs
@@ -1,12 +1,6 @@
 ﻿// Напишите программу, которая принимает 
 // на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 
-     void dudistans3d(resiult)
-    {
-    double resiult = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
-    return resiult;
-    }
-
 Console.WriteLine("Введите координаты первой точки x1");
 int x1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координаты первой точки y1");
@@ -21,8 +15,6 @@ int y2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите координаты второй точки z2");
 int z2 = int.Parse(Console.ReadLine());
 
-//double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
 
-void dudistans3d();
-
-Console.WriteLine(Math.Round(resiult,2));
+Console.WriteLine(Math.Round(d,2));
