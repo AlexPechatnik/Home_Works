84f0cdd1f66a6ae22d3e5a4507e84dc00ecf805f
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-27 11:35:19 +0300

    add_25

---
+++ Home_Works_№25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход два числа А и В, и возводит число А в натуральную степень В.
+Console.WriteLine("Введите число А");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В");
+int num2 = int.Parse(Console.ReadLine());
+int i = 0;
+int result = 1;
+
+while (num2 >= i)
+{
+    result = num1*num1;
+    i++;
+}
+Console.WriteLine(result);
+
