8d449d80e6264644573471c825b8c60d3de57ec9
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-27 16:53:59 +0300

    add_home_works_25

--- Home_Works_№25/Program.cs
+++ Home_Works_№25/Program.cs
@@ -3,12 +3,14 @@ Console.WriteLine("Введите число А");
 int num1 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число В");
 int num2 = int.Parse(Console.ReadLine());
-int i = 0;
-int result = 1;
 
-while (num2 >= i)
+
+int i = 1;
+int result = num1;
+
+while (num2 > i)
 {
-    result = num1*num1;
+    result = result*num1;
     i++;
 }
 Console.WriteLine(result);
