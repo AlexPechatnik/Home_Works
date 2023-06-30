49475605d9b16e2dae63544ea48b741444f86761
Alexander <cg.alexander.kalugin@gmail.com>
2023-06-30 13:23:58 +0300

    add_home_works_36

---
+++ Home_Works_№36/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях. 
+[3, 7, 23, 12] -> 19 
+[-4, -6, 89, 6] -> 0 
+*/
+int[] array = new int[4];
+int sum = 0;
+Console.Write("Массив заполненный случайными числами: ");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write($"{array[i]},");
+}
+for (int i = 0; i < array.Length; i = i + 2)
+{
+    sum = sum + array[i];
+}
+Console.WriteLine(" ");
+Console.Write("Сумма нечетных элеметов массива: ");
+Console.WriteLine(sum);
