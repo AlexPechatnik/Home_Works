63197ffd078ba7710d94ca9c595571aacbbb6dc4
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-10 17:44:11 +0300

    add_50

---
+++ Home_Works_№50/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+*/
+
+int[,] array = new int[,]
+{
+    { 1, 2, 3 },
+    { 4, 5, 6 },
+    { 7, 8, 9 }
+};
+System.Console.WriteLine("Введите позицию элемента: ");
+int Number = int.Parse(Console.ReadLine());
+
+bool found = false;
+int row = -1;
+int col = -1;
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] == Number)
+        {
+            row = i;
+            col = j;
+            found = true;
+            break;
+        }
+    }
+
+    if (found)
+    {
+        break;
+    }
+}
+
+if (found)
+{
+    Console.WriteLine($"Индекс элемента {Number} - [{row}, {col}]");
+}
+else
+{
+    Console.WriteLine("Элемент не найден");
+}
