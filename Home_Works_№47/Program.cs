1d3c3de80aedf05af56d70a48355ae6a2595662b
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-10 16:00:48 +0300

    add_47

---
+++ Home_Works_№47/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Задача 47. Задайте двумерный массив размером m×n, 
+заполненный случайными вещественными числами.
+*/
+
+double[,] array = new double[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+        System.Console.Write(array[i, j] + " ");
+    }
+    System.Console.WriteLine("");
+}
