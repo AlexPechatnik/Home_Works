80de6e3bceb96453daac8829bb3c114da0e2c7b5
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-11 16:45:42 +0300

    add_52

--- Home_Works_№52/Program.cs
+++ Home_Works_№52/Program.cs
@@ -7,20 +7,25 @@
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
 int[,] array = new int[3, 4];
-double n = 0;
-
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
     {
-        array[i, j] = new Random().Next(0, 10);
+        array[i, j] = new Random().Next(1, 5);
         System.Console.Write(array[i, j] + " ");
     }
     System.Console.WriteLine("");
 }
+System.Console.WriteLine("");
 
-for (int i = 0; i < array.GetLength(0); i++)
+for (int i = 0; i < array.GetLength(1); i++)
 {
-
+    double sum = 0;
+    double avResult = 0;
+    for (int j = 0; j < array.GetLength(0); j++)
+    {
+        sum = sum + array[j, i];
+    }
+    avResult = Math.Round(sum / array.GetLength(0),1);
+    System.Console.WriteLine($"Ср. ар. столбца {i}: {avResult}");
 }
-
