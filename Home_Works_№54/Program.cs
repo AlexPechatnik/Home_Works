d0537725a484c4f278133c98888af08b555b0fd2
Alexander Kalugin <cg.alexander.kalugin@gmail.com>
2023-07-14 11:29:23 +0300

    add_54

--- Home_Works_№54/Program.cs
+++ Home_Works_№54/Program.cs
@@ -1,9 +1,11 @@
 ﻿/* Задача 54: Задайте двумерный массив. Напишите программу, 
 которая упорядочит по убыванию элементы каждой строки двумерного массива.
 */
-int[,] array = new int[3, 4];
-int[,] matrix = new int [3,4];
-int max = 0;
+int[,] array = new int[5, 4];
+
+int[] sortedArray = new int[array.Length];
+
+// Заполняем двумерный массив
 for (int i = 0; i < array.GetLength(0); i++)
 {
     for (int j = 0; j < array.GetLength(1); j++)
@@ -15,22 +17,50 @@ for (int i = 0; i < array.GetLength(0); i++)
 }
 System.Console.WriteLine("");
 
-for (int a = 0; a < matrix.GetLength(1); a++)
+// Копирование значений из двумерного массива в одномерный массив
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int d = 0; d < matrix.GetLength(0); d++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
-        for (int i = 0; i < array.GetLength(1); i++)
-        {
-            for (int j = 0; j < array.GetLength(0); j++)
-            {
-                if (array[i, j] >= max)
-                {
-                    max = array[i, j];
-                }
-            }
-        }
-        matrix[a, d] = max;
-        System.Console.WriteLine(matrix[a, d]);
+        sortedArray[i * array.GetLength(1) + j] = array[i, j];
     }
-    System.Console.WriteLine(" ");
 }
+
+// Вывод одномерного массива массива
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(sortedArray[i] + " ");
+}
+System.Console.WriteLine(" ");
+// Сортировка одномерного массива
+for (int i = 0; i < array.Length - 1; i++)
+{
+    for (int j = i + 1; j < array.Length; j++)
+    {
+        if (sortedArray[i] < sortedArray[j])
+        {
+            // Обмен значениями элементов массива
+            int temp = sortedArray[i];
+            sortedArray[i] = sortedArray[j];
+            sortedArray[j] = temp;
+        }
+    }
+}
+// Вывод отсортированного массива
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(sortedArray[i] + " ");
+}
+System.Console.WriteLine(" ");
+System.Console.WriteLine(" ");
+
+// Копирование отсортированных значений обратно в двумерный массив
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = sortedArray[i * array.GetLength(1) + j];
+                System.Console.Write(array[i, j] + " ");
+            }
+            System.Console.WriteLine(" ");
+        }
---
+++ Home_Works_№56/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Исходный двумерный массив
+        int[,] matrix = new int[,]
+        {
+            { 3, 2, 1 },
+            { 6, 5, 4 },
+            { 9, 8, 7 }
+        };
+
+        // Создание нового одномерного массива для сортировки
+        int[] sortedArray = new int[matrix.Length];
+
+        // Копирование значений из двумерного массива в одномерный массив
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                sortedArray[i * matrix.GetLength(1) + j] = matrix[i, j];
+            }
+        }
+
+        // Сортировка одномерного массива в порядке убывания
+        Array.Sort(sortedArray);
+        Array.Reverse(sortedArray);
+
+        // Копирование отсортированных значений обратно в двумерный массив
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                matrix[i, j] = sortedArray[i * matrix.GetLength(1) + j];
+            }
+        }
+
+        // Вывод отсортированного двумерного массива
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
